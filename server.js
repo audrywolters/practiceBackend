f4595d3296992fef8f3afaae6ba3aeb52e13445d
Audry <audrywolters@gmail.com>
2019-09-19 16:00:25 -0500

    webstorm cleaned up code

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ let Todo = require('./todo.model');
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useUnifiedTopology: true,  useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/todos', {useUnifiedTopology: true, useNewUrlParser: true});
 const connection = mongoose.connection;
 
-connection.once('open', function() {
+connection.once('open', function () {
     console.log('MongoDB database connection established successfullllly');
-})
+});
 
 todoRoutes.route('/').get(function (req, res) {
     Todo.find(function (err, todos) {
@@ -59,7 +59,7 @@ todoRoutes.route('/update/:id').post(function (req, res) {
     });
 });
 
-todoRoutes.route('/add').post(function(req, res) {
+todoRoutes.route('/add').post(function (req, res) {
     let todo = new Todo(req.body);
     todo.save()
         .then(todo => {
